4d3813acc7263144111b742e6e71c08176a2f3a5
Fred Willmore <fredwillmore@gmail.com>
2014-09-21 23:06:17 -0700

    getting admin controller working with haml templates for gifts

--- app/controllers/admin/gifts_controller.rb
+++ app/controllers/admin/gifts_controller.rb
@@ -1,2 +1,15 @@
 class Admin::GiftsController < ApplicationController
+
+  def index
+    @gifts = Gift.all
+  end
+
+  def new
+    @gift = Gift.new
+  end
+
+  def edit
+    @gift = Gift.find params[:id]
+  end
+
 end
--- app/models/admin/gift.rb
+++
@@ -1,2 +0,0 @@
-class Admin::Gift < ActiveRecord::Base
-end
